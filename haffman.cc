e19c4f7644668eaf2c5b561df4ee60cb6c340ea1
artemskorypin <55595651+artemskorypin@users.noreply.github.com>
2020-03-07 15:50:01 +0700

    Update haffman.cc

--- haffman.cc
+++ haffman.cc
@@ -5,7 +5,52 @@
 #include <cstring>
 #include <cstdio>
 using namespace std;
-int alf[256], kolafl[256],lenn = 0, flag = 0, k = 0, lenk = 0, rs = 1; 
+/*// Функция "просеивания" через кучу - формирование кучи
+void siftDown(int *mas, int root, int bottom)
+	{
+	  int maxChild; // индекс максимального потомка
+	  int done = 0; // флаг того, что куча сформирована
+	  // Пока не дошли до последнего ряда
+	  while ((root * 2 <= bottom) && (!done)) 
+	  {
+	    if (root * 2 == bottom)    // если мы в последнем ряду,
+	      maxChild = root * 2;    // запоминаем левый потомок
+	    // иначе запоминаем больший потомок из двух
+	    else if (mas[root * 2] > mas[root * 2 + 1])
+	      maxChild = root * 2;
+	    else
+	      maxChild = root * 2 + 1;
+	    // если элемент вершины меньше максимального потомка
+	    if (mas[root] < mas[maxChild]) 
+	    {
+	      int temp = mas[root]; // меняем их местами
+	      mas[root] = mas[maxChild];
+	      mas[maxChild] = temp;
+	      root = maxChild; 
+	    }
+	    else // иначе
+	      done = 1; // пирамида сформирована
+	  }
+	}
+	// Функция сортировки на куче
+int heapSort(int rs,) 
+{
+  // Формируем нижний ряд пирамиды
+  for (int i = (m / 2) - 1; i >= 0; i--)
+    siftDown(mas, i, m - 1);
+	
+ // Просеиваем через пирамиду остальные элементы
+  for (int i = m - 1; i >= 1; i--)
+  {
+    int temp = mas[0];
+    mas[0] = mas[i];
+    mas[i] = temp;
+    siftDown(mas, 0, i - 1);
+  }
+return *mas;
+}
+*/
+int  kolafl[256],lenn = 0, flag = 0, k = 0, lenk = 0, rs = 1; 
 FILE *f = fopen("book.txt",r);//vybor fole na chityvanie
 if (f == NULL)//open file?
     {
@@ -14,26 +59,14 @@ if (f == NULL)//open file?
     }
 else printf("file open\n");
 fscanf(f,"%c",sym);// start chityvaniy
-alf[0] = sym;// alfavit
-kolalf[0] = 1;//chislo povtorov bykvy
+// alfavit
+kolalf[sym] = 1;//chislo povtorov bykvy
 lenn++;//kol-vo symvolov
 while (!feof(f))
 {
         fscanf(f,"%c",sym);
-        lenn++;
-        for(int i = 0, i < rs, i++)//rs - razlichnye symvoly
-            {
-                if (sym == alf[i])
-                    {
-                        kolafl[i]++;
-                        flag = 1;// bykva sovpala 
-                    }
-            }
-        if (flag == 0) 
-        {
-                alf[rs] = sym;
-                kolafl[rs] = 1;
-                rs++;
-        }    
-        flag = 0;//gotovimcy k novoy proverke
+        lenn++;   
+        kolafl[sym]++;
 }    
+
+	
